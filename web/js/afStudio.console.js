3716b3a13092790ba13ab807cf1b5e2d71933a55
Nikolai Babinski <babinski.nikolai@gmail.com>
2010-11-16 20:23:38 +0000

    console maximization - was added maximize button (used Ext.ux.MaximizeTool.js plugin) code refactoring (console component is not longer a "direct" south region panel of afStudio.vp)

---
+++ web/js/afStudio.console.js
@@ -0,0 +1,145 @@
+
+/**
+ * Console
+ * @class afStudio.console
+ * @extends Ext.Panel
+ * @author Radu
+ * reedit by Nikolai
+ */
+afStudio.console = Ext.extend(Ext.Panel, {	
+	
+	/**
+	 * Loads console
+	 */
+	loadConsole : function() {
+		var _this = this;		
+		
+		_this.body.mask('Loading, please Wait...', 'x-mask-loading');
+		
+		Ext.Ajax.request({
+			url: _this.consoleUrl,
+			method: _this.method,
+			params: {
+				command: 'start'
+			},
+			callback: function(options, success, response) {				
+				_this.body.unmask();
+				if (!success) {
+					Ext.Msg.alert('Server-side failure with status code: ' + response.status);
+				}
+				var response = Ext.decode(response.responseText);
+		      	_this.body.dom.innerHTML += response.console;
+		      	_this.body.scroll("bottom", 1000000, true );				
+			}
+			
+		});						
+	} //eo loadConsole
+	
+	/**
+	 * <u>keyup</u> event listener
+	 * @param {Ext.form.TextField} cmdField
+	 * @param {Ext.EventObject} e
+	 */
+	,consoleCommandFieldKeyListener : function(cmdField, e) {		
+		var _this = this,
+			fieldValue = cmdField.getValue(),
+			key = e ? e.getKey() : Ext.EventObject.getKey();				
+		 
+		if (key == Ext.EventObject.ENTER) {			
+			_this.body.mask('Loading, please Wait...', 'x-mask-loading');
+				
+			cmdField.setValue('');
+			
+			if (fieldValue != 'clear') {
+				Ext.Ajax.request({
+					url: _this.consoleUrl,
+					method: _this.method,
+					params:{
+						command: fieldValue
+					},
+					callback: function(options, success, response) {
+				    	var response = Ext.decode(response.responseText);				     					
+				      	afStudio.vp.layout.west.items[0].root.reload();				      	     
+				      	_this.body.unmask();				      
+				      	_this.body.dom.innerHTML += response.console;
+				      	_this.body.scroll("bottom", 1000000, true );
+				    }					
+				});
+			} else if(fieldValue == 'clear') {
+				_this.body.dom.innerHTML = '';
+				_this.body.scroll("bottom", 1000000, true);
+			}
+		}
+	} //eo consoleCommandFieldKeyListener
+	
+	/**
+	 * ExtJS template method
+	 * @private
+	 */
+	,initComponent: function() {
+		Ext.apply(this, Ext.apply(this.initialConfig, this._initCmp()));
+		afStudio.console.superclass.initComponent.apply(this, arguments);
+		this._initEvents();
+	}
+	
+	,_initCmp : function() {
+		var _this = this;
+		
+		var console_cmd_label = new Ext.form.Label({text: 'Enter command: '});
+		
+		var console_cmd_field = new Ext.form.TextField({
+			itemId: 'console_cmd',
+			width: 300,
+			style:'margin-left:5px;',
+			enableKeyEvents: true
+		});
+		
+		var console_cmd_display = new Ext.form.DisplayField({value: '<span style="margin-left:10px;"><b>cmds:</b> ' + afStudioConsoleCommands + '</span>'});
+		
+		var config = {
+			itemId: 'console',
+			title: "Console",
+			iconCls: 'icon-console',
+			height: 200,
+			minHeight: 0,
+			autoScroll: true,
+			tbar: {
+				items:[
+					console_cmd_label,
+					console_cmd_field,
+					console_cmd_display
+				]
+			},
+			html: '',
+			method: 'post',
+			consoleUrl: '/appFlowerStudio/console',
+			bodyStyle: 'background-color:black;font-family: monospace;font-size: 11px;color: #88ff88;',
+			plugins: new Ext.ux.MaximizeTool()
+		};
+		
+		return config;
+	} //eo _initCmp
+	
+	
+	/**
+	 * Initializes events
+	 * @private
+	 */
+	,_initEvents : function() {
+		var _this = this,
+			consoleCmdField = _this.getTopToolbar().getComponent('console_cmd');
+			
+		_this.on({
+			afterrender: function() {
+				_this.loadConsole();
+			}
+		});
+		
+		consoleCmdField.on({
+			keyup : _this.consoleCommandFieldKeyListener.createDelegate(_this)
+		})
+	} //eo _initEvents
+	
+});
+
+Ext.reg('console', afStudio.console);
