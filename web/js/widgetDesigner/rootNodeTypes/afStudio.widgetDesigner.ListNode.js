a4435bf1ac38f829a3a2d3a85a1d0c4778703eaa
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-01-05 10:43:57 +0000

    #741 - Moved up some methods from ListNode to RootNode

--- web/js/widgetDesigner/rootNodeTypes/afStudio.widgetDesigner.ListNode.js
+++ web/js/widgetDesigner/rootNodeTypes/afStudio.widgetDesigner.ListNode.js
@@ -51,7 +51,7 @@ afStudio.widgetDesigner.ListNode = Ext.extend(afStudio.widgetDesigner.ObjectRoot
            id: 'i:fields',
            createChildConstructor: columnNode,
            childNodeId: 'i:column',
-           addChildActionLabel: 'Add field'
+           addChildActionLabel: 'Add column'
         });
 
         return methodNode;
@@ -124,62 +124,5 @@ afStudio.widgetDesigner.ListNode = Ext.extend(afStudio.widgetDesigner.ObjectRoot
             }
         });
         return columnNode;
-    },
-    buildDatasourceNode: function(){
-        var methodNode = this.buildMethodNode();
-        var datasourceNode = new afStudio.widgetDesigner.ContainerNodeBuilder().getConstructor({
-           text: 'Datasource',
-           id: 'i:datasource',
-           createProperties: function(){
-               return [
-                    new afStudio.widgetDesigner.PropertyTypeChoice('type','Type')
-                        .setChoices({
-                            'orm':'orm',
-                            'file':'file',
-                            'static': 'static'
-                        }).create(),
-                   new afStudio.widgetDesigner.PropertyTypeString('i:class','Class').create()
-               ];
-            },
-            createRequiredChilds: function(){
-                return [
-                    new methodNode
-                ];
-            }
-        });
-
-        return datasourceNode;
-    },
-    buildMethodNode: function(){
-        var paramNode = this.buildParamNode();
-        var methodNode = new afStudio.widgetDesigner.CollectionNodeBuilder().getConstructor({
-           text: 'Method',
-           id: 'i:method',
-           createChildConstructor: paramNode,
-           childNodeId: 'i:param',
-           addChildActionLabel: 'Add param',
-           createProperties: function(){
-               return [
-                   new afStudio.widgetDesigner.PropertyTypeString('name','Name').setRequired().create()
-               ];
-           }
-        });
-
-        return methodNode;
-    },
-    buildParamNode: function(){
-        var paramNode = new afStudio.widgetDesigner.ContainerNodeBuilder().getConstructor({
-           text: 'parameter',
-           updateNodeNameFromPropertyId: 'name',
-           createProperties: function(){
-               return [
-                    new afStudio.widgetDesigner.PropertyBaseType('name','Name').setRequired().create(),
-                    new afStudio.widgetDesigner.PropertyBaseType('_content','Value').setRequired().create()
-               ];
-            }
-        });
-        return paramNode;
     }
-
-
 });
--- web/js/widgetDesigner/rootNodeTypes/afStudio.widgetDesigner.ObjectRootNode.js
+++ web/js/widgetDesigner/rootNodeTypes/afStudio.widgetDesigner.ObjectRootNode.js
@@ -14,5 +14,60 @@ afStudio.widgetDesigner.ObjectRootNode = Ext.extend(afStudio.widgetDesigner.Base
     createProperties: function(){
         this.addProperty(new afStudio.widgetDesigner.PropertyBaseType('i:title','Title').create());
         this.addProperty(new afStudio.widgetDesigner.PropertyTypeBoolean('i:description','Description').create());
+    },
+    buildDatasourceNode: function(){
+        var methodNode = this.buildMethodNode();
+        var datasourceNode = new afStudio.widgetDesigner.ContainerNodeBuilder().getConstructor({
+           text: 'Datasource',
+           id: 'i:datasource',
+           createProperties: function(){
+               return [
+                    new afStudio.widgetDesigner.PropertyTypeChoice('type','Type')
+                        .setChoices({
+                            'orm':'orm',
+                            'file':'file',
+                            'static': 'static'
+                        }).create(),
+                   new afStudio.widgetDesigner.PropertyTypeString('i:class','Class').create()
+               ];
+            },
+            createRequiredChilds: function(){
+                return [
+                    new methodNode
+                ];
+            }
+        });
+
+        return datasourceNode;
+    },
+    buildMethodNode: function(){
+        var paramNode = this.buildParamNode();
+        var methodNode = new afStudio.widgetDesigner.CollectionNodeBuilder().getConstructor({
+           text: 'Method',
+           id: 'i:method',
+           createChildConstructor: paramNode,
+           childNodeId: 'i:param',
+           addChildActionLabel: 'Add param',
+           createProperties: function(){
+               return [
+                   new afStudio.widgetDesigner.PropertyTypeString('name','Name').setRequired().create()
+               ];
+           }
+        });
+
+        return methodNode;
+    },
+    buildParamNode: function(){
+        var paramNode = new afStudio.widgetDesigner.ContainerNodeBuilder().getConstructor({
+           text: 'parameter',
+           updateNodeNameFromPropertyId: 'name',
+           createProperties: function(){
+               return [
+                    new afStudio.widgetDesigner.PropertyBaseType('name','Name').setRequired().create(),
+                    new afStudio.widgetDesigner.PropertyBaseType('_content','Value').setRequired().create()
+               ];
+            }
+        });
+        return paramNode;
     }
 });
