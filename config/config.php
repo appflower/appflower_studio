02808c08b29eab511ae54dbf98cb58e00e6014a1
Radu Topala <radu.topala@trisoft.ro>
2011-02-18 15:24:04 +0000

    activating modules automatically

--- config/config.php
+++ config/config.php
@@ -1,2 +1,4 @@
 <?php
 $this->dispatcher->connect('routing.load_configuration', array('afsRouting', 'listenToRoutingLoadConfigurationEvent'));
+
+sfConfig::set('sf_enabled_modules', array_merge(sfConfig::get('sf_enabled_modules'), array('appFlowerStudio', 'afsWidgetBuilder', 'afsDatabaseQuery', 'afsModelGridData')));
