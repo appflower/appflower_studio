7f89e763e20b8c263f88b954886b756c31516799
Radu Topala <radu.topala@trisoft.ro>
2011-02-23 07:27:35 +0000

    modified config for adding modules automatically and filters for disabling csrf

--- config/config.php
+++ config/config.php
@@ -1,15 +1,11 @@
 <?php
 $this->dispatcher->connect('routing.load_configuration', array('afsRouting', 'listenToRoutingLoadConfigurationEvent'));
 
+$modules = afStudioUtil::getDirectories(sfConfig::get('sf_plugins_dir')."/appFlowerStudioPlugin/modules/",true);
+
 sfConfig::set(  'sf_enabled_modules', 
                 array_merge(
                             sfConfig::get('sf_enabled_modules'), 
-                            array(
-                                'appFlowerStudio', 
-                                'afsWidgetBuilder', 
-                                'afsDatabaseQuery', 
-                                'afsModelGridData', 
-                                'afsAuthorize'
-                            )
+                            $modules
                 )
 );
