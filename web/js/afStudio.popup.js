77c2559a145d8267a63af2234d9b8c78649ca458
Milos Milovanovic <milos.silni@gmail.com>
2011-04-05 01:43:15 +0200

    #852 - new welcome screen

--- web/js/afStudio.popup.js
+++ web/js/afStudio.popup.js
@@ -70,3 +70,42 @@ afStudio.Help = Ext.extend(Ext.Window, {
 		this.close();
 	}
 });
+
+afStudio.Welcome = Ext.extend(Ext.Window, { 
+	form: null,
+	initComponent: function(){
+	
+		
+		var config = {
+			title: 'Welcome to AppFlower Studio', width:810,height:520,
+			closable: true,
+	        draggable: true, plain:true,
+	        modal: true, resizable: true,
+	        bodyBorder: false, border: false,
+	        html: '<div id="welcome-box"> </div>',
+	        buttonAlign: 'center'
+		};
+				
+		// apply config
+		Ext.apply(this, Ext.apply(this.initialConfig, config));
+		afStudio.Help.superclass.initComponent.apply(this, arguments);
+		
+		this.getHtmlData();
+	},
+	
+	getHtmlData: function(){
+		Ext.Ajax.request({
+		   url: window.afStudioWSUrls.buildUrlFor('/appFlowerStudio/Welcome'),
+		   failure: function ( result, request) {
+				var obj = Ext.decode(result.responseText);
+				Ext.MessageBox.alert('Failed', result.responseText); 
+		   },
+		   success: function ( result, request) {
+			   var obj = Ext.decode(result.responseText);
+			   Ext.get('welcome-box').update(obj.message);
+		   }
+		});
+
+	},
+	
+});
