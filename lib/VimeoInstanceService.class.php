fda6169c56bd47697db7c053868a225c2661ba33
Milos Milovanovic <milos.silni@gmail.com>
2011-04-05 01:42:15 +0200

    #852 - new welcome screen

---
+++ lib/VimeoInstanceService.class.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class VimeoInstanceService
+{
+	private $url;
+    
+    public function VimeoInstanceService()
+    {
+        $user = sfConfig::get('app_vimeo_user', 'appflower');
+
+        if ($user == '') {
+            throw new Exception('You must set app_vimeo_user in app.yml');
+        }
+        
+        $this->url = 'http://vimeo.com/api/v2/'.$user.'/videos.json';
+    }
+
+    function getDataFromRemoteServer()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+
+        $httpResult = curl_exec($ch);
+
+        $curlError = curl_error($ch);
+
+        if ($curlError != '') {
+            throw new Exception('Curl error occured: ' . $curlError);
+        }
+
+        return json_decode($httpResult, true);
+    }
+
+}
+?>
