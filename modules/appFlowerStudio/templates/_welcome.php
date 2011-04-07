3eee911e68d7d5b45d3949af96b974f53a55e0e8
Milos Milovanovic <milos.silni@gmail.com>
2011-04-07 14:53:33 +0200

    #852 - small changes on welcome popup

--- modules/appFlowerStudio/templates/_welcome.php
+++ modules/appFlowerStudio/templates/_welcome.php
@@ -3,15 +3,15 @@
 
  <div id="studio_popup">
     <div id="studio_popup_primary">
+      <a href="http://vimeo.com/21965153" rel="prettyPhoto" title="AppFlower-003: Installing appflower vmdk on virtual box"> <img src="/appFlowerStudioPlugin/images/studio/bg_popup.png" style="margin-left: 35px; margin-top: 40px;" /> </a>
       <div id="popup_content">
         <h2>Welcome to AppFlower Studio</h2>
-        <p>There is going to be some description tetxt in this area. There is going to be some description tetxt in this area.</p>
-        <a href="http://vimeo.com/21965153" rel="prettyPhoto" title="AppFlower-003: Installing appflower vmdk on virtual box">Start</a>
+        <p>AppFlower Studio makes application development easy and fun. If you are new to AppFlower Studio, you can watch this introduction video now.</p>
       </div>
     </div>
     <div id="studio_popup_secondary">
      <div id="studio_video_tours">
-      <h3>Video Tours</h3>
+      <h3>VIDEO TUTORIALS</h3>
       <ul>
         
           <?php foreach ($data as $video): ?>
@@ -19,8 +19,7 @@
               <a href="<?php echo $video['url']?>" rel="prettyPhoto" title="<?php echo $video['title'] ?>"> <img src="<?php echo $video['thumbnail_small']?>" /> </a>
               <div>
                 <p class="views"><span><?php echo $video['stats_number_of_plays']?> views</span></p>
-                <h5><?php echo substr( $video['title'], 0, 20 ) ?></h5>
-                <span><?php echo substr($video['description'], 0, 25)?></span>
+                <h5><?php echo $video['title'] ?></h5>
               </div>
             </li> 
           <?php endforeach; ?>
@@ -28,7 +27,7 @@
      </ul>
     </div>
     <div id="quick_links">
-      <h3>Quick links</h3>
+      <h3>QUICK LINKS</h3>
       <ul>
         <li><a href="javascript: return false" id="create-project">Create new project</a></li>
         <li><a href="javascript: return false" id="open-project">Open existing project</a></li>
