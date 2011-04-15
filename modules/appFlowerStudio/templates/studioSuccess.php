41edc9105c95466b2e9aeb8dbe8ac4077ceb9eff
Nikolai Babinski <babinski.nikolai@gmail.com>
2011-04-15 10:46:11 +0300

    corrected studioSuccess.php view due to the changes in appflower_engine submodule

--- modules/appFlowerStudio/templates/studioSuccess.php
+++ modules/appFlowerStudio/templates/studioSuccess.php
@@ -11,49 +11,49 @@ window.afStudioWSUrls = new afStudio.WSUrlsClass();
 <script type="text/javascript" src="/appFlowerPlugin/js/custom/BorderLayoutOverride.js"></script>
 
 <script type="text/javascript" src="/appFlowerPlugin/js/custom/gridUtil.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/overrides/Override.Ext.data.SortTypes.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/overrides/Override.Ext.form.Field.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/overrides/Override.Fixes.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/plugins/Ext.ux.Notification.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/layout/AccordionLayoutSetActiveItem.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/portal/Ext.ux.MaximizeTool.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/form/Ext.ux.form.Combo.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/form/Ext.ux.plugins.HelpText.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/overrides/Override.Ext.data.SortTypes.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/overrides/Override.Ext.form.Field.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/overrides/Override.Fixes.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/plugins/Ext.ux.Notification.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/layout/AccordionLayoutSetActiveItem.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/portal/Ext.ux.MaximizeTool.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/form/Ext.ux.form.Combo.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/form/Ext.ux.plugins.HelpText.js"></script>
 
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/plugins/Ext.ux.plugins.RealtimeWidgetUpdate.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid/Ext.ux.GridColorView.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid/Ext.ux.GroupingColorView.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid/Ext.ux.Grid.GroupingStoreOverride.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid/RowExpander.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/plugins/Ext.ux.plugins.RealtimeWidgetUpdate.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid/Ext.ux.GridColorView.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid/Ext.ux.GroupingColorView.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid/Ext.ux.Grid.GroupingStoreOverride.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid/RowExpander.js"></script>
 <script type="text/javascript" src="/appFlowerPlugin/js/custom/cheatJS.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/rowactionsImm/js/Ext.ux.GridRowActions.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/form/lovcombo-1.0/js/Ext.ux.form.LovCombo.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/treegrid/Ext.ux.CheckboxSelectionModel.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/rowactionsImm/js/Ext.ux.GridRowActions.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/form/lovcombo-1.0/js/Ext.ux.form.LovCombo.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/treegrid/Ext.ux.CheckboxSelectionModel.js"></script>
 
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/menu/EditableItem.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/menu/ComboMenu.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/menu/RangeMenu.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/GridFilters.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/DrillFilter.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/RePositionFilters.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/SaveSearchState.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/FilterInfo.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/filter/Filter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/menu/EditableItem.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/menu/ComboMenu.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/menu/RangeMenu.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/GridFilters.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/DrillFilter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/RePositionFilters.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/SaveSearchState.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/FilterInfo.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/filter/Filter.js"></script>
 
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/filter/BooleanFilter.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/filter/ComboFilter.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/filter/DateFilter.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/filter/ListFilter.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/filter/NumericFilter.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid-filtering/ux/grid/filter/StringFilter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/filter/BooleanFilter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/filter/ComboFilter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/filter/DateFilter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/filter/ListFilter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/filter/NumericFilter.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid-filtering/ux/grid/filter/StringFilter.js"></script>
 
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/portal/sample-grid.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/portal/sample-grid.js"></script>
 <script type="text/javascript" src="/appFlowerPlugin/js/custom/portalsJS.js"></script>
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/form/Ext.ux.ClassicFormPanel.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/form/Ext.ux.ClassicFormPanel.js"></script>
 
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/grid/Ext.ux.grid.RowEditor.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/grid/Ext.ux.grid.RowEditor.js"></script>
 
-<script type="text/javascript" src="/appFlowerPlugin/extjs-3/examples/form/groupingcombobox/Ext.ux.form.GroupingComboBox.js"></script>
+<script type="text/javascript" src="/appFlowerPlugin/extjs-3/plugins/form/groupingcombobox/Ext.ux.form.GroupingComboBox.js"></script>
 
 <script type="text/javascript">
 var afStudioConsoleCommands='<?php echo afStudioConsole::getCommands(false); ?>';
@@ -216,14 +216,14 @@ foreach ($afStudioJsExtensions as $afStudioJsExtension)
 <link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/resources/css/ext-all.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/resources/css/xtheme-blue.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/css/my-extjs.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/examples/rowactionsImm/css/Ext.ux.GridRowActions.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/examples/rowactionsImm/css/icons.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/examples/form/lovcombo-1.0/css/Ext.ux.form.LovCombo.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/examples/grid-filtering/resources/style.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/examples/portal/portal.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/plugins/rowactionsImm/css/Ext.ux.GridRowActions.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/plugins/rowactionsImm/css/icons.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/plugins/form/lovcombo-1.0/css/Ext.ux.form.LovCombo.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/plugins/grid-filtering/resources/style.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/plugins/portal/portal.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
 
-<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/examples/grid/Ext.ux.grid.RowEditor.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/plugins/grid/Ext.ux.grid.RowEditor.css" />
 
 <link rel="stylesheet" type="text/css" media="screen" href="/appFlowerStudioPlugin/css/afStudio.css" />
 
@@ -231,7 +231,7 @@ foreach ($afStudioJsExtensions as $afStudioJsExtension)
 <link rel="stylesheet" type="text/css" media="screen" href="/appFlowerStudioPlugin/js/filetree/css/filetree.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="/appFlowerStudioPlugin/js/filetree/css/icons.css" />
 
-<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/examples/form/groupingcombobox/Ext.ux.form.GroupingComboBox.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/appFlowerPlugin/extjs-3/plugins/form/groupingcombobox/Ext.ux.form.GroupingComboBox.css" />
 
 <?php 
 $appFlowerStudioPluginCssPath = sfConfig::get('sf_root_dir').'/plugins/appFlowerStudioPlugin/web/css/';
