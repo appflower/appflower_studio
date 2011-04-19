639e6fe2946fee1c97d99922c19006813a1ab159
Nikolai Babinski <babinski.nikolai@gmail.com>
2011-04-19 15:55:58 +0300

    #883 - fixed bug inside simple list view when widget has only one column

--- web/js/components/widgetDesigner/designer/list/SimpleListView.js
+++ web/js/components/widgetDesigner/designer/list/SimpleListView.js
@@ -46,23 +46,43 @@ afStudio.wd.list.SimpleListView = Ext.extend(Ext.grid.GridPanel, {
 			
 			var columns = [];
 			
-			for (var i = 0; i < clm.length; i++) {
-				var c = clm[i];				
+			if (Ext.isArray(clm)) {
+				for (var i = 0; i < clm.length; i++) {
+					var c = clm[i];				
+					columns.push({
+						header:   c.label,
+						width:    c.width ? c.width : this.columnWidth,
+						hidden:   c.hidden ? c.hidden.boolean() : false,
+						hideable: c.hideable ? c.hideable.boolean() : true,
+						fixed:    c.resizable ? c.resizable.boolean() : false
+					});				
+				}
+				for (var i = columns.length - 1; i <= this.maxColumns; i++) {
+					columns.push({
+						header: this.columnName,
+						width: this.columnWidth,
+						hidden: true,
+						uninit: true
+					});
+				}
+				
+			} else {
 				columns.push({
-					header:   c.label,
-					width:    c.width ? c.width : this.columnWidth,
-					hidden:   c.hidden ? c.hidden.boolean() : false,
-					hideable: c.hideable ? c.hideable.boolean() : true,
-					fixed:    c.resizable ? c.resizable.boolean() : false
-				});				
-			}
-			for (var i = columns.length - 1; i <= this.maxColumns; i++) {
-				columns.push({
-					header: this.columnName,
-					width: this.columnWidth,
-					hidden: true,
-					uninit: true
+					header:   clm.label,
+					width:    clm.width ? clm.width : this.columnWidth,
+					hidden:   clm.hidden ? clm.hidden.boolean() : false,
+					hideable: clm.hideable ? clm.hideable.boolean() : true,
+					fixed:    clm.resizable ? clm.resizable.boolean() : false					
 				});
+				
+				for (var i = 1; i <= this.maxColumns; i++) {
+					columns.push({
+						header: this.columnName,
+						width: this.columnWidth,
+						hidden: true,
+						uninit: true
+					});
+				}				
 			}
 		}
 
