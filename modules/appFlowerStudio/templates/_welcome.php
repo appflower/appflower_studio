1043d5c8acd41c726ada557b7bfb21bdd859e3b0
Milos Milovanovic <milos.silni@gmail.com>
2011-05-16 14:42:09 +0200

    #913 - welcome window new design

--- modules/appFlowerStudio/templates/_welcome.php
+++ modules/appFlowerStudio/templates/_welcome.php
@@ -1,33 +1,33 @@
 <?php echo stylesheet_tag('/appFlowerStudioPlugin/css/welcome.css') ?>
 <?php echo stylesheet_tag('/appFlowerStudioPlugin/css/prettyPhoto.css') ?>
-
- <div id="studio_popup">
+  
+<div id="studio_popup">
     <div id="studio_popup_primary">
-      <a href="http://vimeo.com/21965153" rel="prettyPhoto" title="AppFlower-003: Installing appflower vmdk on virtual box"> <img src="/appFlowerStudioPlugin/images/studio/bg_popup.png" style="margin-left: 35px; margin-top: 40px;" /> </a>
       <div id="popup_content">
         <h2>Welcome to AppFlower Studio</h2>
         <p>AppFlower Studio makes application development easy and fun. If you are new to AppFlower Studio, you can watch this introduction video now.</p>
+        <a href="http://vimeo.com/21965153" rel="prettyPhoto" title="AppFlower-003: Installing appflower vmdk on virtual box">Start</a>
       </div>
     </div>
     <div id="studio_popup_secondary">
      <div id="studio_video_tours">
-      <h3>VIDEO TUTORIALS</h3>
+      <h3>Video Tours</h3>
       <ul>
-        
+      
           <?php foreach ($data as $video): ?>
             <li>
-              <a href="<?php echo $video['url']?>" rel="prettyPhoto" title="<?php echo $video['title'] ?>"> <img src="<?php echo $video['thumbnail_small']?>" /> </a>
-              <div>
-                <p class="views"><span><?php echo $video['stats_number_of_plays']?> views</span></p>
-                <h5><?php echo $video['title'] ?></h5>
-              </div>
-            </li> 
+	          <img src="<?php echo $video['thumbnail_small']?>" alt="<?php echo $video['title'] ?>" width="57" height="57">
+	          <div>
+	            <h5><a href="<?php echo $video['url']?>" rel="prettyPhoto" title="<?php echo $video['title'] ?>"><?php echo $video['title'] ?></a></h5>
+	            <p class="views"><span><?php echo $video['stats_number_of_plays']?> views</span></p>
+	          </div>
+	        </li>
           <?php endforeach; ?>
-            
+      
      </ul>
     </div>
     <div id="quick_links">
-      <h3>QUICK LINKS</h3>
+      <h3>Quick links</h3>
       <ul>
         <li><a href="javascript: return false" id="create-project">Create new project</a></li>
         <li><a href="javascript: return false" id="open-project">Open existing project</a></li>
