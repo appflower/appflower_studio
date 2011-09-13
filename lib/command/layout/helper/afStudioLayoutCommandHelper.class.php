423b176f77a49695c9da7c1730076577147033ac
Sergey Startsev <startsev.sergey@gmail.com>
2011-09-13 19:38:39 +0300

    #1056 - changed getWidget functionality - for now gives full info about widget

--- lib/command/layout/helper/afStudioLayoutCommandHelper.class.php
+++ lib/command/layout/helper/afStudioLayoutCommandHelper.class.php
@@ -2,19 +2,20 @@
 /**
  * Studio Layout Command Helper class 
  * 
+ * @package appFlowerStudio
  * @author Sergey Startsev <startsev.sergey@gmail.com>
  */
 class afStudioLayoutCommandHelper
 {
     /**
      * Returns ExtJS data 
+     * 
      * @return array the tree structure
      * @author Sergey Startsev
      */
-    public static function processGetList($aPageList)
+    public static function processGetList(Array $aPageList)
     {
-    	$tree = array();
-        
+        $tree = array();
         foreach ($aPageList as $app => $aPage) {
             
             $treeNode['text'] = $app;
@@ -23,11 +24,11 @@ class afStudioLayoutCommandHelper
             if (count($aPage) > 0) {
                 foreach ($aPage as $page) {
                     $treeNode['children'][] = array(
-                        'text' => $page['text'],
-                    	'iconCls' => 'icon-layout',
-                        'xmlPath' => $page['xmlPath'],
-                        'leaf' => true,
-                    	'type' => 'page'
+                        'text'      => $page['text'],
+                        'iconCls'   => 'icon-layout',
+                        'xmlPath'   => $page['xmlPath'],
+                        'leaf'      => true,
+                        'type'      => 'page'
                     );
                 }
             } else {
@@ -37,8 +38,8 @@ class afStudioLayoutCommandHelper
             
             $tree[] = $treeNode;
         }
-    	
-    	return $tree;
+        
+        return $tree;
     }
     
     /**
@@ -51,36 +52,35 @@ class afStudioLayoutCommandHelper
      * @return array
      * @author Sergey Startsev
      */
-    public static function processGetWidgetList($modules, $params, $name, $type)
+    public static function processGetWidgetList(Array $modules, Array $params, $name, $type)
     {
         $aExtWidgets = array();
         
         foreach ($modules as $module) {
-			if (count($params[$module]['xml_names']) > 0) {	
-			    $children = array(
-                    'text' => $module,
-                    'type' => 'module',
-                    'app' => $name
-    			);
+            if (count($params[$module]['xml_names']) > 0) {	
+                $children = array(
+                    'text'  => $module,
+                    'type'  => 'module',
+                    'app'   => $name,
+                    'leaf'  => false,
+                );
                 
-                $children['leaf'] = false;
-                
-				foreach ($params[$module]['xml_names'] as $xk => $xmlName) {
-					$children['children'][] = array(
+                foreach ($params[$module]['xml_names'] as $xk => $xmlName) {
+                    $children['children'][] = array(
                         'app'           => $name,
                         'module'        => $module,
-					    'widget'        => str_replace('.xml', '', $xmlName),
+                        'widget'        => str_replace('.xml', '', $xmlName),
                         'widgetUri'     => $module.'/'.str_replace('.xml', '', $xmlName),
                         'type'          => 'xml',
                         'text'          => $xmlName,
                         'securityPath'  => $params[$module]['security_path'],
                         'xmlPath'       => $params[$module]['xml_paths'][$xk],
                         'actionPath'    => $params[$module]['action_path'],
-                        'leaf'          => true
-					);
-				}
-				$aExtWidgets['children'][] = $children;
-			} 
+                        'leaf'          => true,
+                    );
+                }
+                $aExtWidgets['children'][] = $children;
+            } 
         }
         
         if (!empty($aExtWidgets)) {
