de66dc2aae67b5b40340289d0c492884537ca021
Nikolai Babinski <babinski.nikolai@gmail.com>
2011-09-19 13:50:29 +0300

    edit view gui researching D&D

--- web/js/components/widgetDesigner/gui/edit/EditView.js
+++ web/js/components/widgetDesigner/gui/edit/EditView.js
@@ -3,4 +3,33 @@ Ext.ns('afStudio.wd.edit');
 
 afStudio.wd.edit.EditView = Ext.extend(Ext.Panel, {
 
+	initComponent : function() {
+
+		Ext.apply(this, {
+
+//			layout : 'fit',
+
+			items: [
+			{
+				xtype: 'platform.basement',
+//				height: 800,
+				items: [
+				{
+					columnWidth:.5,
+					style: 'padding:10px',
+					items: [{html: 'A'}, {html: 'B'}]
+				},{
+					columnWidth:.5,
+					style: 'padding:10px',
+					items: [{html: 'C'}]
+				}]
+			}],
+
+			plugins: new Ext.ux.WidgetFieldDragZone()
+
+		});
+
+		afStudio.wd.edit.EditView.superclass.initComponent.apply(this, arguments);		
+	}
+	
 });
