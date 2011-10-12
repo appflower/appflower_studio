dc7fb649f0d97a000fc438330c10cb71ce1f5bd7
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-10-12 21:27:35 +0200

    Revert "Added afGuardPlugin to exclude lists for model and plugins navigation tree"
    This reverts commit 61b76f4872018ed3ae2cd1d66eca266bc4258563.

--- lib/command/plugin/helper/afStudioPluginCommandHelper.class.php
+++ lib/command/plugin/helper/afStudioPluginCommandHelper.class.php
@@ -24,7 +24,6 @@ class afStudioPluginCommandHelper extends afBaseStudioCommandHelper
         'sfPropelORMPlugin',
         'sfPropelSqlDiffPlugin',
         'sfTaskExtraPlugin',
-        'afGuardPlugin'
     );
     
     /**
