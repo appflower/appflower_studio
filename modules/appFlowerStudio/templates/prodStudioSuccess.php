630c97c6e56864a32fcf466f765c2d1d03ec2206
Nikolai Babinski <babinski.nikolai@gmail.com>
2011-10-12 10:49:50 +0300

    added requirejs lib, ace editor uses it

--- modules/appFlowerStudio/templates/prodStudioSuccess.php
+++ modules/appFlowerStudio/templates/prodStudioSuccess.php
@@ -7,6 +7,7 @@
 <script type="text/javascript" src="/appFlowerPlugin/extjs-3/ext-all.js"></script>
 
 <script type="text/javascript" src="/appFlowerStudioPlugin/cache/af.js"></script>
+
 <script type="text/javascript">
 var afStudioConsoleCommands = '<?php echo afStudioConsole::getCommands(false); ?>';
 var afStudioUser = <?php echo html_entity_decode($afStudioUser); ?>;
@@ -18,7 +19,9 @@ var afStudioHost = {
 var afProjectInPath = '<?php echo $projectInPath; ?>';
 var afTemplateConfig = <?php echo json_encode(afStudioUtil::getTemplateConfig()); ?>;
 </script>
+
 <script type="text/javascript" src="/appFlowerStudioPlugin/cache/afStudio.js"></script>
+<script src="/appFlowerStudioPlugin/js/ace/require.js" data-ace-base="/appFlowerStudioPlugin/js/ace/src" type="text/javascript" charset="utf-8"></script> 
 
 <script type="text/javascript">
 	Ext.onReady(afStudio.init, afStudio);
