3b7d918c73b4f3bd30e66f255ef193c5cf4c1bb8
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2011-10-28 13:05:27 +0200

    tiny refactorization in main studio php templates

---
+++ modules/appFlowerStudio/templates/_studioSettings.php
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+var afStudioConsoleCommands = '<?php echo afStudioConsole::getCommands(false); ?>';
+var afStudioUser = <?php echo html_entity_decode($afStudioUser); ?>;
+var afStudioHost = { 
+	name: '<?php echo afStudioConsole::getInstance()->getUnameShort();?>',
+	user: '<?php echo afStudioConsole::getInstance()->getWhoami();?>' 
+};
+<?php
+    $projectPath = sfConfig::get('sf_root_dir');
+    $projectInPath = explode('/',$projectPath);
+    unset($projectInPath[count($projectInPath)-1]);
+    $projectInPath = implode('/',$projectInPath);
+?>
+var afProjectInPath = '<?php echo $projectInPath; ?>';
+var afTemplateConfig = <?php echo json_encode(afStudioUtil::getTemplateConfig()); ?>;
+</script>
--- modules/appFlowerStudio/templates/prodStudioSuccess.php
+++ modules/appFlowerStudio/templates/prodStudioSuccess.php
@@ -7,19 +7,7 @@
 <script type="text/javascript" src="/appFlowerPlugin/extjs-3/ext-all.js"></script>
 
 <script type="text/javascript" src="/appFlowerStudioPlugin/cache/af.js"></script>
-
-<script type="text/javascript">
-var afStudioConsoleCommands = '<?php echo afStudioConsole::getCommands(false); ?>';
-var afStudioUser = <?php echo html_entity_decode($afStudioUser); ?>;
-var afStudioHost = { 
-	name: '<?php echo afStudioConsole::getInstance()->getUnameShort();?>',
-	user: '<?php echo afStudioConsole::getInstance()->getWhoami();?>' 
-};
-<?php $projectPath = sfConfig::get('sf_root_dir'); $projectInPath = explode('/',$projectPath); unset($projectInPath[count($projectInPath)-1]); $projectInPath = implode('/',$projectInPath);?>
-var afProjectInPath = '<?php echo $projectInPath; ?>';
-var afTemplateConfig = <?php echo json_encode(afStudioUtil::getTemplateConfig()); ?>;
-</script>
-
+<?php include_partial('studioSettings', array('afStudioUser' => $afStudioUser)); ?>
 <script type="text/javascript" src="/appFlowerStudioPlugin/cache/afStudio.js"></script>
 <script src="/appFlowerStudioPlugin/js/ace/require.js" data-ace-base="/appFlowerStudioPlugin/js/ace/src" type="text/javascript" charset="utf-8"></script> 
 
