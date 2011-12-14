2a76fc40f96766816c9788418715fe902fab12ed
Sergey Startsev <startsev.sergey@gmail.com>
2011-12-15 00:49:43 +0200

    #1304 - added missed exception class

---
+++ lib/command/project/helper/exception/afStudioProjectCommandHelperException.class.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Project command helper exception class
+ *
+ * @package appFlowerStudio
+ * @author Sergey Startsev <startsev.sergey@gmail.com>
+ */
+class afStudioProjectCommandHelperException extends Exception {}
