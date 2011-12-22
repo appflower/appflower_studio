dfe8c5277eb1773ff70fd939aa3decf9abd656b9
Nikolai Babinski <babinski.nikolai@gmail.com>
2011-12-22 16:41:35 +0200

    #1298 - created layout of upload import file form

--- web/js/components/models/importForm/UploadForm.js
+++ web/js/components/models/importForm/UploadForm.js
@@ -15,6 +15,21 @@ afStudio.models.UploadForm = Ext.extend(Ext.FormPanel, {
     url : afStudioWSUrls.modelListUrl,
     
     /**
+     * @cfg {String} 
+     */
+    
+    /**
+     * Uploading import file. 
+     * @property importFile
+     * @type {Ext.ux.form.FileUploadField}
+     */
+    /**
+     * Container of additional fields required for import process. 
+     * @property extraFields 
+     * @type {Ext.Container}
+     */
+    
+    /**
      * Initializes component.
      * @private
      * @return {Object} The configuration object
@@ -26,16 +41,97 @@ afStudio.models.UploadForm = Ext.extend(Ext.FormPanel, {
             items: [
             {
                 xtype: 'fileuploadfield',
+                ref: 'importFile',
                 fieldLabel: 'Import file',
                 emptyText: 'Select import file...',
                 allowBlank: false,
                 blankText: 'File is required',
                 width: 400,
                 name: 'file'
+            },{
+                xtype: 'displayfield',
+                submitValue: false,
+                html: 'Supported files: CSV, XLS, ODS, YML'
+            },{
+                xtype: 'container',
+                ref: 'extraFields',
+                anchor: '100% 90%',
+                style: 'margin-top: 10px;',
+                layout: 'card',
+                //TODO implement switching
+                activeItem: 1,
+                defaults: {
+                    layout: 'form',
+                    border: false
+                },
+                items: [
+                {
+                    //CSV
+                    items: [
+                    {
+                        xtype: 'checkbox',
+                        fieldLabel: 'Use headers',
+                        name: 'has_headers'
+                    },{
+                        xtype: 'combo',
+                        store: [[',', ','], [';', ';'], [':', ':'], ['|', '|'], ['\t', 'horizontal tab (\\t)']],
+                        mode: 'local',
+                        triggerAction: 'all',
+                        forceSelection: true,
+                        fieldLabel: 'Delimeter',
+                        value: ',',
+                        hiddenName: 'delimeter',
+                        name: 'delimeter'
+                    },{
+                        xtype: 'combo',
+                        store: [[',', ','], ['', 'empty string ("")']],
+                        mode: 'local',
+                        triggerAction: 'all',
+                        forceSelection: true,
+                        fieldLabel: 'Enclosure',
+                        value: '',
+                        hiddenName: 'enclosure',
+                        name: 'enclosure'
+                    },{
+                        xtype: 'common.modelscombo',
+                        allowBlank: false
+                    }]
+                },{
+                    //XLS and ODS
+                    items: [
+                    {
+		                xtype: 'label',
+		                html: '<u>Supported spreadsheets</u>: <i>MS Excel 2007 (xlsx), MS Excel 5, 95, 2000 (xls), OpeOffice Cals (ods)</i>'
+                    },{
+                        xtype: 'checkbox',
+                        fieldLabel: 'Use headers',
+                        name: 'has_headers'
+                    },{
+                        xtype: 'checkbox',
+                        fieldLabel: 'Import raw values',
+                        value: false,
+                        name: 'raw'
+                    },{
+                        xtype: 'textfield',
+                        fieldLabel: 'Import selected worksheets',
+                        width: 345,
+                        name: 'worksheet',
+                        plugins: [Ext.ux.plugins.HelpText], 
+                        helpType: "comment",
+                        helpText: 'All worksheets will be imported unless is not specified the list separated<br /> by commas, i.e. sheetA, sheetB'
+                    },{
+                        xtype: 'checkbox',
+                        fieldLabel: 'Use worksheets as models',
+                        name: 'worksheets_as_models'
+                    },{
+                        xtype: 'common.modelscombo'
+                    }]
+                }]
             }],
             buttons: [
             {
                 text: 'Import',
+                itemId: 'import',
                 iconCls: 'icon-accept',
                 formBind: true,
                 scope: this,
@@ -63,29 +159,31 @@ afStudio.models.UploadForm = Ext.extend(Ext.FormPanel, {
      * @private
      */
     afterRender : function() {
-//        var fSm = this.fixturesGrid.getSelectionModel(),
-//            fStore = this.fixturesGrid.getStore();
-//        
-//        this.addEvents(
-//            /**
-//             * @event importfixtures Fires when fixtures were successfully imported
-//             * @param {Array} fixtures The fixtures which were imported
-//             */
-//            'importfixtures'
-//        );
-//        
-//        fSm.on('selectionchange', function(){
-//            var importBtn = this.getImportButton();
-//            fSm.hasSelection() ? importBtn.enable() : importBtn.disable();
-//        }, this);
-//        
-//        if (this.autoLoadFixtures === true) {
-//            fStore.load();
-//        }
+        var f = this.importFile,
+            ct = this.extraFields;
+        
+        console.log(f, ct);
+        
+        this.addEvents(
+            /**
+             * @event importfile Fires when file was successfully imported
+             */
+            'importfile'
+        );
+        
+        f.on('fileselected', this.onFileSelected, this);
         
         afStudio.models.UploadForm.superclass.afterRender.call(this);
     },
 
+    /**
+     * Importing file {@link #importFile} <u>fileselected</u> event handler.
+     * @param {Ext.ux.form.FileUploadField} fileCmp The file-upload component
+     * @param {String} file The selected file's name
+     */
+    onFileSelected : function(fileCmp, file) {
+    },
+    
     /**
      * Returns import button.
      * @return {Ext.Button} import button
@@ -97,28 +195,10 @@ afStudio.models.UploadForm = Ext.extend(Ext.FormPanel, {
     },
     
     /**
-     * Imports fixtures files.
+     * Uploads and imports data file.
      * @protected
      */
-    importFixtures : function() {
-        var fSm = this.fixturesGrid.getSelectionModel(),
-            recs = fSm.getSelections(),
-            append = this.appendChbox.getValue(),
-            files = [];
-        
-        afStudio.xhr.executeAction({
-            url: this.url,
-            params: {
-                cmd: 'importData',
-                append: append
-            },
-            //mask: "Importing...",
-            scope: this,
-            run: function(response, opts) {
-                
-                this.fireEvent('importfixtures', opts.params.remote_files);
-            }
-        });
+    importData : function() {
     }
     
 });
