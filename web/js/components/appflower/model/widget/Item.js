a0b936f89f05748a1b3faf83d6d59f6988957d62
Nikolai Babinski <babinski.nikolai@gmail.com>
2012-01-24 13:36:01 +0200

    #1365 - changed i:item tag, to have ability to set up static text

--- web/js/components/appflower/model/widget/Item.js
+++ web/js/components/appflower/model/widget/Item.js
@@ -6,7 +6,7 @@ afStudio.model.widget.Item = Ext.extend(afStudio.model.Node, {
 
 	tag: afStudio.ModelNode.ITEM,
 	
-	_content : null,
+	_content : {name: '_content', type: 'string', required: true},
 	
 	properties : [
         {name: 'value', type: 'token', required: true}
