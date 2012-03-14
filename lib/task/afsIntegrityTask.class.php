8fbbf0ff1b99fd001a5c2c4eff77f5727b2d5b88
Sergey Startsev <startsev.sergey@gmail.com>
2012-03-14 21:58:38 +0200

    #1324 - created task that will show integrity status

---
+++ lib/task/afsIntegrityTask.class.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload/UniversalClassLoader.class.php';
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+    'AppFlower\Studio'  => dirname(__DIR__) . '/vendor',
+));
+$loader->register();
+
+use AppFlower\Studio\Integrity as Integrity;
+
+/**
+ * Checks integrity of project
+ * 
+ * @package     appFlowerStudio
+ * @subpackage  task
+ * @author      Sergey Startsev <startsev.sergey@gmail.com>
+ */
+class afsPermissionsTask extends sfBaseTask
+{
+    /**
+     * @see sfTask
+     */
+    protected function configure()
+    {
+        $this->addOptions(array(
+            new sfCommandOption('rules', 'r', sfCommandOption::PARAMETER_OPTIONAL, 'List of rules separated by comma that should be executed', ''),
+        ));
+        
+        $this->namespace = 'afs';
+        $this->name = 'integrity';
+        $this->briefDescription = 'Checks integrity of project';
+        
+        $this->detailedDescription = <<<EOF
+The [afs:integrity|INFO] task shows problems that may exists in project:
+
+  [./symfony afs:integrity|INFO]
+EOF;
+    }
+    
+    /**
+     * @see sfTask
+     */
+    protected function execute($arguments = array(), $options = array())
+    {
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        
+        $integrity = Integrity\Integrity::create();
+        (empty($options['rules'])) ? $integrity->check() : call_user_func_array(array($integrity, 'check'), explode(',', $options['rules']));
+        
+        if ($integrity->isImpaired()) {
+            throw new sfCommandException($integrity->render(Integrity\Renderer\Helper::TYPE_TEXT));
+        }
+        
+        $this->logSection('done', 'Project integrity is not impaired');
+    }
+    
+}
